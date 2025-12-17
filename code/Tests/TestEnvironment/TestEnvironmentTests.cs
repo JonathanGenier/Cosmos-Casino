bd24603a3ccf5314240fbecfe9dc1c9004e2581f
Jonathan <jo_app@outlook.com>
2025-12-17 10:36:44 -0500

    Configure project testing infrastructure. Divided code structure into the following: Game, Core, Tests Game handling all Godot logic, node, anything that uses Godot. Core handling all Data Logic, doesn't handle Godot at all. Tests handles testing for Core.

---
+++ code/Tests/TestEnvironment/TestEnvironmentTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace CosmosCasino.Tests.TestEnvironment
+{
+    [TestFixture]
+    internal class TestEnvironmentTests
+    {
+        [Test]
+        public void TestFrameWork_IsOperational()
+        {
+            Assert.Pass("Test Framework is operational.");
+        }
+    }
+}
