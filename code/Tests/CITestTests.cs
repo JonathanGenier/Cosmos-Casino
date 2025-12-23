d843ac6c49145fca0b5523ee880fd133877a0148
Jonathan <jo_app@outlook.com>
2025-12-23 01:56:00 -0500

    test

---
+++ code/Tests/CITestTests.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmosCasino.Tests
+{
+    [TestFixture]
+    internal class CITestTests
+    {
+
+        [Test]
+        public void TestTests()
+        {
+            bool no = false;
+
+            Assert.That(no, Is.True);
+        }
+    }
+}
